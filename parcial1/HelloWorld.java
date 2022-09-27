482d5836c4a31aca6b1e3b23ff94415f21274029
nicole2309 <2hermanoswichester@gmail.com>
2022-09-27 17:52:45 -0500

    se agrego holamundo.java

---
+++ parcial1/HelloWorld.java
@@ -0,0 +1,10 @@
+// HelloWorld.java
+// Author:  Nicole Bautista
+// Date: sep 26, 2022
+
+class HelloWorld {
+   public static void main(String[] args) {
+       System.out.println("Hello, World !");
+   }
+}
+
