4f36fdcec15c87c12bbe805d9e0eb8cc860b0010
nicole2309 <112724830+nicole2309@users.noreply.github.com>
2022-11-08 16:46:04 -0600

    Update Promedio.java

--- parcial1/Tarea4/Promedio.java
+++ parcial1/Tarea4/Promedio.java
@@ -1,48 +1,23 @@
-import javax.swing.JOptionPane;
+// Permitir la captura de n números y calcular su promedio
+
+import java.util.Scanner;
 
 public class Promedio {
+public static void main(String args[]){
 
-    public static void prome(int iteracion) {
+try (Scanner sn = new Scanner(System.in)) {
+System.out.print("Introduzca un numero: ");
+int n=sn.nextInt();
+int i = 1;
+double num = 0;
 
-        double [] array = new double [iteracion];
-        double entrada;
-
-        System.out.print("El promedio: ");
-
-        for (int i = 0; i<array.length; i++) {
-
-            entrada = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa los números a promediar: "));
-            array[i] = entrada;
-
-            if (i<array.length-1) {
-
-                System.out.print(array[i]+ " + ");
-
-            }else if (i == array.length-1) {
-
-                System.out.print(array[i]+ " = ");
-
-            } 
-        
-        }
-         
-        entrada = 0;
-
-        for (int i=0;i<array.length;i++) {
-
-            entrada += array[i];
-
-        }
-
-        System.out.print(entrada/array.length+ ".");
- 
-    }
-    
-    public static void main(String[] args) {
-
-        int iteracion = Integer.parseInt(JOptionPane.showInputDialog(null, "¿Cuántos números desea promediar?"));
-        prome(iteracion);
-
-    }
-
+for (i = 1; i <= n; i=i+1){
+System.out.println(i);
+num += i;
 }
+
+System.out.println("El promedio es: "+ num/n);
+}
+}
+}
+
