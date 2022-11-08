f0c05cc015fd3e7c597a7390b487c0c1693a3047
nicole2309 <112724830+nicole2309@users.noreply.github.com>
2022-11-08 16:47:00 -0600

    Update Triangulo.java

--- parcial1/Tarea4/Triangulo.java
+++ parcial1/Tarea4/Triangulo.java
@@ -1,34 +1,27 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Triangulo {
+public static void main(String[] args) {
 
-    public static void trian(int entrada_datos) {
+Scanner piramide=new Scanner(System.in);
+System.out.print("Ingresar cantidad de filas: ");
+int n=piramide.nextInt();
+piramide.close();
 
-        if (entrada_datos>=0 && entrada_datos<=9) {
-            System.out.println();
+System.out.println();
+for (int i = 1; i < n+1; i++ ){
+for (int j = 1; j < n; j++){
+System.out.print(" ");
+}
 
-          for(int altura = 1; altura<=entrada_datos; altura++){
-        
-            for(int blancos = 1; blancos<=entrada_datos-altura; blancos++){
-                System.out.print(" ");
-            }
- 
-            for(int asteriscos=1; asteriscos<=(altura*2)-1; asteriscos++){
-                System.out.print("*");
-            }
-            System.out.println();
-          }
+for (int l = 9; l >= (i); l=l-1){
+System.out.print(" ");
+}
 
-        }else {
-
-            JOptionPane.showMessageDialog(null, "Erro \n El numero que usted ingreso no es valido");
-        }
-    }
-    public static void main(String[] args) {
-
-       int entrada_datos = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa en numeros el tamaño que deseas que tenga \n la impresión del triangulo en un rango de 0 a 9: "));
-       trian(entrada_datos);
-
-    }
-
+for (int l = 1; l <= (i*2)-1; l++){
+System.out.print("*");
+}
+System.out.println();
+}
+}
 }
